be1f9169772011fd187d398b526248b2990e5a72
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-06-07 16:18:14 -0300

    feat: date and time set using switch

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-var resultado = document.getElementById("resultado")
-var myDate = new Date()
-var today = myDate.getDate()
-var semana = myDate.getDay()
-var ano = myDate.getFullYear()
+let resultado = document.getElementById("resultado");
+let myDate = new Date();
+let today = myDate.getDate();
+let semana = myDate.getDay();
+let ano = myDate.getFullYear();
 
-
-
-
-
-
-resultado.innerHTML = `Hoje é dia ${today} de ${semana} de ${ano}`
-
-
-
+switch (semana) {
+  case 1:
+    resultado.innerHTML = `Hoje é Segunda-feira , dia ${today}  de ${ano}.`;
+    break;
+  case 2:
+    resultado.innerHTML = `Hoje é Terça-feira, dia ${today} de   ${ano}.`;
+    break;
+  case 3:
+    resultado.innerHTML = `Hoje é Quarta-feira, dia  ${today} de   ${ano}.`;
+    break;
+  case 4:
+    resultado.innerHTML = `Hoje é  Quinta-feira, dia ${today}  de ${ano}.`;
+    break;
+  case 5:
+    resultado.innerHTML = `Hoje é  Sexta-feira,  dia  ${today}  de ${ano}.`;
+    break;
+  case 6:
+    resultado.innerHTML = `Hoje é  Sábado, dia ${today}  de ${ano}.`;
+    break;
+  case 7:
+    resultado.innerHTML = `Hoje é Domingo, dia ${today}  de ${ano}.`;
+    break;
+  default:
+    console.log(`Sorry, we are out of ${semana}.`);
+}
